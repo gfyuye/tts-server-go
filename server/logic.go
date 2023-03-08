0319de16481de2b5114cc6dd610791c38e0c58b1
gfyuye <107809384+gfyuye@users.noreply.github.com>
2023-03-08 10:31:21 +0800

    Update logic.go

--- server/logic.go
+++ server/logic.go
@@ -3,7 +3,7 @@ package server
 import (
 	"bytes"
 	"encoding/json"
-	"github.com/jing332/tts-server-go/tts"
+	"github.com/gfyuye/tts-server-go/tts"
 	log "github.com/sirupsen/logrus"
 	"strconv"
 	"strings"
