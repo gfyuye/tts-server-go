b82044ed38007ad1604050e2add417e9b6b3205e
gfyuye <107809384+gfyuye@users.noreply.github.com>
2023-03-08 10:07:18 +0800

    Update main.go

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	logformat "github.com/antonfisher/nested-logrus-formatter"
-	go get "github.com/gfyuye/tts-server-go/server"
+	get "github.com/gfyuye/tts-server-go/server"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
